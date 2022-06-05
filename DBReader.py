24e650fae6079646b0f23d0580f13774490725eb
redone <elkaboussi@pm.me>
2022-06-05 02:22:26 +0100

    Some improvements

--- DBReader.py
+++ DBReader.py
@@ -1,10 +1,14 @@
 import sqlite3
+
+
 def ReturnDB():
-        try:
-            c.execute("SELECT * FROM gen_keys")
-            print(c.fetchall())
-        except Exception as e:
-            print(("ERROR :2: {}").format(str(e)))
+    try:
+        c.execute("SELECT * FROM gen_keys")
+        print(c.fetchall())
+    except Exception as e:
+        print(f"ERROR :2: {str(e)}")
+
+
 class Read:
     def __init__(self, db):
         try:
@@ -13,4 +17,4 @@ class Read:
             c = connection.cursor()
             ReturnDB()
         except Exception as e:
-           print(("ERROR :1: {}").format(str(e)))
+            print(f"ERROR :1: {str(e)}")
--- test_run.py
+++ test_run.py
@@ -1,4 +1,5 @@
 import KeyDBGen, DBReader
+
 Run = KeyDBGen.MyData()
 Run.GenKey("First Name", "Last Name")
 READ = DBReader.Read("temp_key.db")
